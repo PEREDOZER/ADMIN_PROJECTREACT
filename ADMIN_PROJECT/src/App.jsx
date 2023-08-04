d032d1d73c906c97011ccfa7f9cdf92ae4c71021
PEREDOZER <141398116+PEREDOZER@users.noreply.github.com>
2023-08-04 12:38:06 +0500

    ADMIN_PROJECT
    ustoz menu bomayapti

---
+++ ADMIN_PROJECT/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navigation from './Components/NavigationTemplate/Navigation';
+
+function App() {
+   return (
+<div className='App'>
+<Navigation />
+
+{/* <Main /> */}
+
+
+</div>
+  );
+}
+
+export default App
